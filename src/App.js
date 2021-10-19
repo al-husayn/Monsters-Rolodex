ee529d764eb3ce7884ec41e257d6da30493ced30
Abubakar Al-Hussein <al-hussein.abubakar@amalitech.org>
2021-10-19 16:16:59 +0000

    card list component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import './App.css';
+import './App.css'; 
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -13,10 +14,14 @@ class App extends Component {
     .then(response => response.json()
     .then(users=> this.setState({monsters:users})));
   }
-  render() {
-    return ( 
+  render() { 
+    return (
       <div className="App">
-      {this.state.monsters.map(monster => (<h1>{monster.name}</h1>))}
+        <CardList>
+          {this.state.monsters.map((monster) => (
+            <h1>{monster.name}</h1>
+          ))}  
+        </CardList>
       </div>
     );
   }
