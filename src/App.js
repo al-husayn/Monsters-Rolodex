06b1e180b1074e0a8c4c6b49687af330c8e69742
Abubakar Al-Hussein <al-hussein.abubakar@amalitech.org>
2021-10-20 11:28:40 +0000

    Card Component

--- src/App.js
+++ src/App.js
@@ -17,10 +17,8 @@ class App extends Component {
   render() { 
     return (
       <div className="App">
-        <CardList>
-          {this.state.monsters.map((monster) => (
-            <h1>{monster.name}</h1>
-          ))}  
+        <CardList monsters={this.state.monsters}>
+            
         </CardList>
       </div>
     );
